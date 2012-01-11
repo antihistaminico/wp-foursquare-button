450f94bdd219d4a69e90971031ecdb00f68fc83d
antihistaminico <magrio.ag@gmail.com>
2012-01-11 07:33:47 +0000

    Button size is now eligible

--- vCard.php
+++ vCard.php
@@ -1,4 +1,4 @@
-<a href="https://foursquare.com/intent/venue.html" class="fourSq-widget" <?php echo $data['buttontype'] == 'wide' ? 'data-variant="wide"' : NULL ?>>Save to foursquare</a>
+<a href="https://foursquare.com/intent/venue.html" class="fourSq-widget" <?php echo $meta['buttontype'] == 'wide' ? 'data-variant="wide"' : NULL ?>>Save to foursquare</a>
 <div style="display:none;" class='locInfo vCard'>
 	<p class="fn"><?php echo $meta['venue']; ?></p>
 </div>
